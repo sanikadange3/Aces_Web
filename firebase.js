e2a02f807989dedc5e3d35d66f1b01f83fe57731
Sanika Dange <sanikadange3@gmail.com>
2026-03-29 23:10:18 +0530

    Remove node_modules and update Firebase setup

--- firebase.js
+++ firebase.js
@@ -1,21 +1,19 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/9.22.1/firebase-app.js";
-import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.22.1/firebase-analytics.js";
-import { getAuth } from "https://www.gstatic.com/firebasejs/9.22.1/firebase-auth.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/9.22.1/firebase-firestore.js";
-import { getStorage } from "https://www.gstatic.com/firebasejs/9.22.1/firebase-storage.js";
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyDKtmy1nAmtidzDjkCpxflvp4hpjlHf9eU",
-    authDomain: "aceswebsite.firebaseapp.com",
-    projectId: "aceswebsite",
-    storageBucket: "aceswebsite.firebasestorage.app",
-    messagingSenderId: "220508792496",
-    appId: "1:220508792496:web:8abe967ad07f27aa873d75",
-    measurementId: "G-0BP6QE745Z"
+    apiKey: import.meta.env.VITE_API_KEY,
+    authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+    projectId: import.meta.env.VITE_PROJECT_ID,
+    storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+    messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
+    appId: import.meta.env.VITE_APP_ID,
+    measurementId: import.meta.env.VITE_MEASUREMENT_ID
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
 const auth = getAuth(app);
